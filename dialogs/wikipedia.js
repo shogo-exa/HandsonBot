b5822fe8b08ae1d65a4020811fb2d51aac840801
hagimori <shogo-hagimori@exa-corp.co.jp>
2017-10-13 13:47:34 +0900

    wikiにリクエストを出してレスポンスをログに出すところまでのプロトタイプを実装

--- dialogs/wikipedia.js
+++ dialogs/wikipedia.js
@@ -1,14 +1,53 @@
 const builder = require('botbuilder');
 const loger = require('./log.js');
+const request = require('request');
+const async = require('async');
+const log = require('../log');
+
 
 var lib = new builder.Library('wikipedia');
 
-lib.dialog('/', [
-    (session, args, next) => {
+const triggerRegExp = ["(.+)を(教えて | おしえて)$", "^wiki( |　)(.+)"]
 
-    },
-]);
+lib.dialog('search', [
+    (session, args, next) => {
+        const searchWord = ExtractionSearchWord(session.message);
+        sendRequest(searchWord, session);
+    }
+]).triggerAction({
+    matches: [RegExp(triggerRegExp[0]), RegExp(triggerRegExp[1])]
+});
+
+function ExtractionSearchWord(message) {
+    for (var regExp of triggerRegExp) {
+        message = message.replace(RegExp(regExp), "");
+    }
+    return message;
+}
 
 module.exports.createLibrary = function () {
     return lib.clone();
 };
+
+function sendRequest(word, session) {
+    var options = {
+        url: 'https://ja.wikipedia.org/w/api.php',
+        method: 'get',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        json: true,
+        form: {
+            format: 'json',
+            action: 'query',
+            prop: 'extracts',
+            exintro: '',
+            explaintext: '',
+            titles: word
+        }
+    }
+    request(options, function (error, response, body) {
+        log.console("wiki_response", response);
+        log.console("wiki_body", body);
+    })
+}
