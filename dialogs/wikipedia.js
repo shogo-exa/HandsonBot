27814e6f2ef45518a4b835ad0a50d88066fd0efa
hagimori <shogo-hagimori@exa-corp.co.jp>
2017-10-16 10:00:12 +0900

    リクエストに使用するライブラリを変更

--- dialogs/wikipedia.js
+++ dialogs/wikipedia.js
@@ -1,6 +1,7 @@
 const builder = require('botbuilder');
 const log = require('../log.js');
-const request = require('request');
+// const request = require('request');
+const request = require('superagent');
 const async = require('async');
 
 
@@ -8,13 +9,59 @@ var lib = new builder.Library('wikipedia');
 
 const triggerRegExp = ["(.+)について(教えて|おしえて)$", "^wiki( |　)(.+)"]
 const ExtractionRegExp = ["について(教えて|おしえて)$", "^wiki( |　)"]
+const apiInfo = {
+    url: 'https://ja.wikipedia.org/w/api.php',
+    method: 'get',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    json: true,
+    form: {
+        action: "opensearch",
+    }
+}
 
 lib.dialog('search', [
     (session, args, next) => {
+        // 検索ワードを取得する
         const searchWord = ExtractionSearchWord(session.message.text);
-        var result = getWikipedia(searchWord, session);
-        log.console("wiki_result", result);
-        session.send(result);
+
+        // リクエスト情報を作成
+        request.get('https://ja.wikipedia.org/w/api.php')
+            .query({
+                format: 'json',
+                action: 'opensearch',
+                search: searchWord
+            }).end((err, res) => {
+                log.log("superagent", res);
+            })
+        // var option = apiInfo;
+        // option.form.search = searchWord;
+        // async.series([(next) => {
+        //     // wikipediaのapiへリクエストを送信
+        //     request(option, function (error, response, body) {
+        //         if (error) {
+        //             next(error, null);
+        //         } else {
+        //             // log.console("wiki_response", response);
+        //             // log.console("wiki_body", body);
+        //             next(null, response)
+        //         }
+        //     })
+
+        // }, (next) => {
+        //     // 結果を取得して確認
+        //     if (err) {
+        //         session.send("えらー");
+        //         log.console("wikipedia_error", err)
+        //     } else {
+        //         log.console("wiki_results", results);
+        //     }
+
+        // }], (err, results) => {
+        //     // 結果をユーザーに送信
+        //     session.send(results);
+        // })
     }
 ]).triggerAction({
     matches: [RegExp(triggerRegExp[0]), RegExp(triggerRegExp[1])]
@@ -28,46 +75,6 @@ function ExtractionSearchWord(message) {
 
     return message;
 }
-
-function getWikipedia(word, session) {
-    console.log("はじまり")
-    var options = {
-        url: 'https://ja.wikipedia.org/w/api.php',
-        method: 'get',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        json: true,
-        form: {
-            action: "opensearch",
-            search: word
-        }
-    }
-    var result;
-    async.series([(next) => {
-        request(options, function (error, response, body) {
-            if (error) {
-                next(error, null);
-            } else {
-                // log.console("wiki_response", response);
-                // log.console("wiki_body", body);
-                next(null, response)
-            }
-        })
-    }], (err, results) => {
-        if (err) {
-            session.send("えらー");
-            log.console("wikipedia_error", err)
-        } else {
-            log.console("wiki_results", results);
-            result = results[0];
-        }
-    })
-    console.log("おわり")
-    return result;
-}
-
-
 
 module.exports.createLibrary = function () {
     return lib.clone();
