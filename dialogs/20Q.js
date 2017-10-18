02e76341cf5dc6a01e4f7e0df6033f6fa451c338
hagimori <shogo-hagimori@exa-corp.co.jp>
2017-10-18 11:36:32 +0900

    dialogの指定方法を修正

--- dialogs/20Q.js
+++ dialogs/20Q.js
@@ -63,7 +63,7 @@ lib.dialog('20Q', [
     matches: [RegExp(triggerRegExp)],
     confirmPrompt: "20Qゲームを始めますか？"
 
-}).beginDialogAction("20QHelpAction", "help_20Q", {
+}).beginDialogAction("20QHelpAction", "Help:20q_help", {
     matches: /^help$/i,
 
 }).cancelAction('cancel20Q', "キャンセルしました", {
