f4976775b7de3296ca11ef028ef962e67b5d071c
hagimori <shogo-hagimori@exa-corp.co.jp>
2017-10-18 10:14:20 +0900

    ユーザーの選択方法を変更

--- dialogs/help.js
+++ dialogs/help.js
@@ -5,25 +5,32 @@ var lib = new builder.Library('Help');
 
 const triggerRegExp = "^help$"
 
+const func = {
+    "wiki": "wiki",
+    "20Q": "wiki",
+    "weather": "weather"
+}
+
 lib.dialog('help_global', [
     (session, args, next) => {
-        var replyData = new builder.Message(session);
-        replyData.attachmentLayout(builder.AttachmentLayout.carousel);
-        replyData.attachments([
-            new builder.HeroCard(session)
-            .title('私はこんなことが出来ます。')
-            .subtitle('help')
-            .text('更に機能の詳細を知りたい場合は選択してください')
-            .buttons([
-                builder.CardAction.imBack(session, 'wiki', 'wikipedia検索'),
-                builder.CardAction.imBack(session, '20Q', '20Qで遊ぶ'),
-                builder.CardAction.imBack(session, 'weather', '今日の天気')
-            ])
-        ]);
-        builder.Prompts.text(session, replyData);
+        // var replyData = new builder.Message(session);
+        // replyData.attachmentLayout(builder.AttachmentLayout.carousel);
+        // replyData.attachments([
+        //     new builder.HeroCard(session)
+        //     .title('私はこんなことが出来ます。')
+        //     .subtitle('help')
+        //     .text('更に機能の詳細を知りたい場合は選択してください')
+        //     .buttons([
+        //         builder.CardAction.imBack(session, 'wiki', 'wikipedia検索'),
+        //         builder.CardAction.imBack(session, '20Q', '20Qで遊ぶ'),
+        //         builder.CardAction.imBack(session, 'weather', '今日の天気')
+        //     ])
+        // ]);
+        builder.Prompts.choice(session, "更に機能の詳細を知りたい場合は選択してください", replyData);
     },
     (session, res, next) => {
-        switch (res.response) {
+        log.log("ユーザーが選んだ", res.response)
+        switch (res.response.entity) {
             case "wiki":
                 session.replaceDialog("help_wiki")
                 break;
@@ -43,6 +50,7 @@ lib.dialog('help_global', [
 
 lib.dialog("help_wiki", [
     (session, args, next) => {
+        log.log("wiki_help", "")
         session.send("Wikipediaでキーワードの概要を検索します。");
         session.send("wiki ボット");
         session.send("と入力して頂くと、ボットとは何かを私が検索します。");
@@ -52,11 +60,13 @@ lib.dialog("help_wiki", [
 ])
 lib.dialog("help_20Q", [
     (session, args, next) => {
+        log.log("20q_help", "")
         session.endDialog("工事中(´・ω・｀)")
     }
 ])
 lib.dialog("help_weather", [
     (session, args, next) => {
+        log.log("weather_help", "")
         session.endDialog("工事中(´・ω・｀)")
     }
 ])
